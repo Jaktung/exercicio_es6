d9e999138bd6825e938348c0565c8a140f507267
luiz <luiz@exemplo.com>
2024-06-18 12:17:50 -0300

    exercicio concluido

---
+++ src/alunosAprovados.js
@@ -0,0 +1,16 @@
+const alunos = [
+    { nome: "Luiz", nota: 8 },
+    { nome: "Roberto", nota: 4 },
+    { nome: "Ana", nota: 2 },
+    { nome: "Maria", nota: 6 },
+    { nome: "Lucas", nota: 10 },
+];
+
+function filtraAlunosPorNota(alunos, media) {
+    return alunos.filter(aluno => aluno.nota >= media);
+}
+
+const alunosAprovados = filtraAlunosPorNota(alunos, 6);
+
+
+console.log(alunosAprovados);
